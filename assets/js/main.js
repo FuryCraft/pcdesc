c4efbd941bfbd47ec64ad430f1c6f17baf11abb3
Fury_Craft <gaming.tv.79.64@gmail.com>
2022-10-01 10:31:41 +0200

    Added a footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Changement de thème (code provenant d'une source externe)
 const LOCAL_STORAGE_KEY = "toggle-bootstrap-theme";
 const LOCAL_META_DATA = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
 // you can change this url as needed
@@ -33,4 +34,25 @@ function enableDarkTheme() {
 function disableDarkTheme() {
   DARK_STYLE_LINK.setAttribute("href", "");
   THEME_TOGGLER.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+}
+
+// Revenir au haut de la page (code provenant de W3C)
+// Get the button:
+let mybutton = document.getElementById("goTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
