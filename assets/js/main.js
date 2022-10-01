efa5d3ac4960a65b1b61d6aff9fa145a50171618
Fury_Craft <gaming.tv.79.64@gmail.com>
2022-10-01 17:08:03 +0200

    Added a custom scrollbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // Changement de thème (code provenant d'une source externe)
-const LOCAL_STORAGE_KEY = "toggle-bootstrap-theme";
+const LOCAL_STORAGE_KEY = 'toggle-bootstrap-theme';
 const LOCAL_META_DATA = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
 // you can change this url as needed
-const DARK_THEME_PATH = "assets/css/dark-style.css";
-const DARK_STYLE_LINK = document.getElementById("dark-theme-style");
-const THEME_TOGGLER = document.getElementById("theme-toggler");
+const DARK_THEME_PATH = 'assets/css/dark-style.css';
+const DARK_STYLE_LINK = document.getElementById('dark-theme-style');
+const THEME_TOGGLER = document.getElementById('theme-toggler');
 let isDark = LOCAL_META_DATA && LOCAL_META_DATA.isDark;
 // check if user has already selected dark theme earlier
 if (isDark) {
-  enableDarkTheme();
+	enableDarkTheme();
 } else {
-  disableDarkTheme();
+	disableDarkTheme();
 }
 /**
  * Apart from toggling themes, this will also store user's theme preference in local storage.
@@ -18,52 +18,55 @@ if (isDark) {
  *
  */
 function toggleTheme() {
-  isDark = !isDark;
-  if (isDark) {
-    enableDarkTheme();
-  } else {
-    disableDarkTheme();
-  }
-  const META = { isDark };
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(META));
+	isDark = !isDark;
+	if (isDark) {
+		enableDarkTheme();
+	} else {
+		disableDarkTheme();
+	}
+	const META = { isDark };
+	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(META));
 }
 function enableDarkTheme() {
-  DARK_STYLE_LINK.setAttribute("href", DARK_THEME_PATH);
-  THEME_TOGGLER.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+	DARK_STYLE_LINK.setAttribute('href', DARK_THEME_PATH);
+	THEME_TOGGLER.innerHTML = `<i class="fa-solid fa-moon"></i>`;
 }
 function disableDarkTheme() {
-  DARK_STYLE_LINK.setAttribute("href", "");
-  THEME_TOGGLER.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+	DARK_STYLE_LINK.setAttribute('href', '');
+	THEME_TOGGLER.innerHTML = `<i class="fa-solid fa-sun"></i>`;
 }
 
 // Revenir au haut de la page (code provenant de W3C)
 // Get the button:
-let mybutton = document.getElementById("goTop");
+let mybutton = document.getElementById('goTop');
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-  scrollFunction();
+	scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+	if (
+		document.body.scrollTop > 20 ||
+		document.documentElement.scrollTop > 20
+	) {
+		mybutton.style.display = 'block';
+	} else {
+		mybutton.style.display = 'none';
+	}
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 // Chargement de la page, ajoute un cercle qui tourne le temps que la page charge
-const preloader = document.querySelector(".preloader-wrapper");
-const loading = document.querySelector("body");
+const preloader = document.querySelector('.preloader-wrapper');
+const loading = document.querySelector('body');
 
-window.addEventListener("load", function () {
-  preloader.classList.add("fade-out"); // On dit de jouer l'animation de fondu
-  loading.classList.remove("notLoaded"); // On dit d'afficher toute la page
+window.addEventListener('load', function () {
+	preloader.classList.add('fade-out'); // On dit de jouer l'animation de fondu
+	loading.classList.remove('notLoaded'); // On dit d'afficher toute la page
 });
