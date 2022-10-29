46fd5ec17dad9ad41c1d5247da298e68c633f814
Fury_Craft <gaming.tv.79.64@gmail.com>
2022-10-29 14:40:57 +0200

    New Go Top design

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,24 +79,22 @@ document.addEventListener('click', function (e) {
 /* ###################################################################### */
 
 // Revenir au haut de la page (code provenant de W3C)
-
-let mybutton = document.getElementById('goTop');
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-	scrollFunction();
-};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		mybutton.style.display = 'block';
-	} else {
-		mybutton.style.display = 'none';
-	}
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 20) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	// scroll body to 0px on click
+	$('#back-to-top').click(function () {
+		$('body,html').animate(
+			{
+				scrollTop: 0,
+			},
+			40,
+		);
+		return false;
+	});
+});
